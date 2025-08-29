22958e326376a01a27e650610e1bc0f288293748
Akinori Yoshiba <akinori.official@gmail.com>
2025-08-29 12:30:05 +0900

    リンク設定

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 "use client";
 import React from 'react';
+import Link from 'next/link'; 
 
 // ★ propsに selectedYear と changeYear を追加
 const Header = ({ onLogout }) => {
@@ -8,14 +9,14 @@ const Header = ({ onLogout }) => {
     <header className="app-header">
       <div className="header-main-row">
         <nav className="header-nav">
-          <a href="./" className="header-link">注文入力</a>
-          <a href="./change" className="header-link">注文変更</a>
-          <a href="./dashboard" className="header-link">注文一覧</a>
-          <a href="./netacounts" className="header-link">ネタ数</a>
-           <a href="./netachange" className="header-link">ネタ変更</a>
-          <a href="./allocations" className="header-link">割り当て</a>
-          <a href="./settings" className="header-link">設定</a>
-          <a href="./Log" className="header-link">変更ログ</a>
+          <Link href="./" className="header-link">注文入力</Link>
+          <Link href="./change" className="header-link">注文変更</Link>
+          <Link href="./dashboard" className="header-link">注文一覧</Link>
+          <Link href="./netacounts" className="header-link">ネタ数</Link>
+          <Link href="./netachange" className="header-link">ネタ変更</Link>
+          <Link href="./allocations" className="header-link">割り当て</Link>
+          <Link href="./settings" className="header-link">設定</Link>
+          <Link href="./Log" className="header-link">変更ログ</Link>
         </nav>
         <button onClick={onLogout} className="logout-btn">
           ログアウト
